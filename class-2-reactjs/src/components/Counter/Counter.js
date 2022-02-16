32ac5f63a78064e8b2d3a34d1dafe3e322732561
Adnan Irfan <adnanshurta@gmail.com>
2022-02-16 14:49:21 +0500

    Updated Counter component

--- class-2-reactjs/src/components/Counter/Counter.js
+++ class-2-reactjs/src/components/Counter/Counter.js
@@ -1,9 +1,12 @@
 function Counter(props) {
+  const increment = () => props.setValue(props.value + 1);
+  const decrement = () => props.setValue(props.value - 1);
+
   return (
     <>
       <h1>{props.value}</h1>
-      <button onClick={() => props.setValue(props.value + 1)}>Increment</button>
-      <button onClick={() => props.setValue(props.value - 1)}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </>
   );
 }
